53364ee6da88e3943265b3014bb872e9bf36e477
Winter <2678683235@qq.com>
2025-03-23 10:50:28 +0800

    添加项目文件。

---
+++ mydll/CMainDialog.cpp
@@ -0,0 +1,52 @@
+﻿// CMainDialog.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "mydll.h"
+#include "afxdialogex.h"
+#include "CMainDialog.h"
+
+
+// CMainDialog 对话框
+
+IMPLEMENT_DYNAMIC(CMainDialog, CDialogEx)
+
+CMainDialog::CMainDialog(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+{
+
+}
+
+CMainDialog::~CMainDialog()
+{
+}
+
+void CMainDialog::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CMainDialog, CDialogEx)
+	ON_BN_CLICKED(IDC_BUTTON1, &CMainDialog::OnBnClickedButton1)
+	ON_BN_CLICKED(IDC_BUTTON2, &CMainDialog::OnBnClickedButton2)
+END_MESSAGE_MAP()
+
+
+// CMainDialog 消息处理程序
+
+HMODULE h_DLL = NULL;
+void CMainDialog::OnBnClickedButton1()
+{
+	CFileDialog fileDlg(TRUE, _T("动态库DLL|*.DLL"), NULL, OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT,
+		_T("动态库DLL|*.DLL|所有文件(*.*)|*.*||"));
+	fileDlg.DoModal();
+	h_DLL = LoadLibrary(fileDlg.GetPathName());
+	// TODO: 在此添加控件通知处理程序代码
+}
+
+void CMainDialog::OnBnClickedButton2()
+{
+	FreeLibrary(h_DLL);
+	// TODO: 在此添加控件通知处理程序代码
+}
---
+++ mydll/mydll.cpp
@@ -0,0 +1,71 @@
+﻿// mydll.cpp: 定义 DLL 的初始化例程。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "mydll.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+#include "CMainDialog.h"
+
+//
+//TODO:  如果此 DLL 相对于 MFC DLL 是动态链接的，
+//		则从此 DLL 导出的任何调入
+//		MFC 的函数必须将 AFX_MANAGE_STATE 宏添加到
+//		该函数的最前面。
+//
+//		例如: 
+//
+//		extern "C" BOOL PASCAL EXPORT ExportedFunction()
+//		{
+//			AFX_MANAGE_STATE(AfxGetStaticModuleState());
+//			// 此处为普通函数体
+//		}
+//
+//		此宏先于任何 MFC 调用
+//		出现在每个函数中十分重要。  这意味着
+//		它必须作为以下项中的第一个语句:
+//		出现，甚至先于所有对象变量声明，
+//		这是因为它们的构造函数可能生成 MFC
+//		DLL 调用。
+//
+//		有关其他详细信息，
+//		请参阅 MFC 技术说明 33 和 58。
+//
+
+// CmydllApp
+
+BEGIN_MESSAGE_MAP(CmydllApp, CWinApp)
+END_MESSAGE_MAP()
+
+
+// CmydllApp 构造
+
+CmydllApp::CmydllApp()
+{
+	// TODO:  在此处添加构造代码，
+	// 将所有重要的初始化放置在 InitInstance 中
+}
+
+
+// 唯一的 CmydllApp 对象
+
+CmydllApp theApp;
+CMainDialog mydialog;
+DWORD WINAPI ShowDialog(LPARAM lpData)
+{
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());
+	mydialog.DoModal();
+	FreeLibraryAndExitThread(theApp.m_hInstance, 123456);
+}
+
+// CmydllApp 初始化
+
+BOOL CmydllApp::InitInstance()
+{
+	CWinApp::InitInstance();
+	::CreateThread(0, 0, (LPTHREAD_START_ROUTINE)ShowDialog, 0, 0, 0);
+	return TRUE;
+}
